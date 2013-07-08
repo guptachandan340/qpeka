db22fa9724d4b0b70e02f482a261c96f2e6235e2
Ankita Malani <anki546.malani@gmail.com>
2013-07-08 18:28:49 +0530

    created managers classes

---
+++ QPEKA_UI/src/com/qpeka/managers/user/CategoryManager.java
@@ -0,0 +1,24 @@
+package com.qpeka.managers.user;
+
+import com.qpeka.db.handler.CategoryHandler;
+
+public class CategoryManager {
+private static CategoryManager instance= null;
+CategoryHandler categoryHandler = new CategoryHandler();
+
+public CategoryManager() {
+	super();
+}
+
+public CategoryManager getInstance() {
+	return(instance == null ? instance = new CategoryManager() : instance);
+}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ QPEKA_UI/src/com/qpeka/managers/user/CountryManager.java
@@ -0,0 +1,23 @@
+package com.qpeka.managers.user;
+
+import com.qpeka.db.handler.CountryHandler;
+
+public class CountryManager {
+private static CountryManager instance =null;
+CountryHandler countryHandler = new CountryHandler();
+ public CountryManager() {
+	 super();
+ }
+ 
+ public CountryManager getInstance() {
+	 return (instance == null ? instance = new CountryManager() : instance);
+ }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
